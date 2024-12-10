371b78ffd93446a83cc991a74d1d8cedddb343ee
Lourens Schep <lourensschep@gmail.com>
2024-12-10 13:16:08 +0100

    Fix status field when switching to future

--- packages/edit-site/src/components/post-edit/index.js
+++ packages/edit-site/src/components/post-edit/index.js
@@ -103,7 +103,7 @@ function PostEditForm( { postType, postId } ) {
 				edits.status &&
 				edits.status !== 'future' &&
 				editedRecord?.status === 'future' &&
-				new Date( record.date ) > new Date()
+				new Date( editedRecord.date ) > new Date()
 			) {
 				edits.date = null;
 			}
